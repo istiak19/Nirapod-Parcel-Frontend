e1613689d17c5fd54b1cc03a72f412b581db2c0a
istiak19 <istiak15-14128@diu.edu.bd>
2025-08-21 22:28:16 +0600

    navbar updated

--- src/redux/features/auth/auth.api.ts
+++ src/redux/features/auth/auth.api.ts
@@ -16,6 +16,14 @@ export const authApi = baseApi.injectEndpoints({
                 data: userInfo
             })
         }),
+        logout: builder.mutation({
+            invalidatesTags: ["USER"],
+            query: (userInfo) => ({
+                url: "/auth/logout",
+                method: "POST",
+                data: userInfo
+            })
+        }),
         sentOtp: builder.mutation({
             query: (userInfo) => ({
                 url: "/otp/send",
@@ -33,4 +41,4 @@ export const authApi = baseApi.injectEndpoints({
     })
 });
 
-export const { useRegisterMutation, useLoginMutation, useSentOtpMutation, useVerifyOtpMutation } = authApi;
+export const { useRegisterMutation, useLoginMutation, useLogoutMutation, useSentOtpMutation, useVerifyOtpMutation } = authApi;
