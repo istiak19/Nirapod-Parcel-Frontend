a243c9cb1bde7ee276d7f300a05c6c41deb2c62c
istiak19 <istiak15-14128@diu.edu.bd>
2025-08-23 08:40:39 +0600

    parcel part redux part add

--- src/redux/features/parcel/parcel.api.ts
+++ src/redux/features/parcel/parcel.api.ts
@@ -3,20 +3,36 @@ import { baseApi } from "@/redux/baseApi";
 export const parcelApi = baseApi.injectEndpoints({
     endpoints: (builder) => ({
         track: builder.query({
+            providesTags: ["PARCEL"],
             query: (trackId) => ({
                 url: `/parcels/track/${trackId}`,
                 method: "GET"
             })
         }),
-        // updateUser: builder.mutation({
-        //     invalidatesTags: ["USER"],
-        //     query: ({ id, userInfo }) => ({
-        //         url: `/user/${id}`,
-        //         method: "POST",
-        //         data: userInfo
-        //     })
-        // }),
+        getAllParcel: builder.query({
+            providesTags: ["PARCEL"],
+            query: () => ({
+                url: "/parcels",
+                method: "GET"
+            })
+        }),
+        parcelStatusChange: builder.mutation({
+            invalidatesTags: ["PARCEL"],
+            query: ({ id, parcelInfo }) => ({
+                url: `/parcels/status/${id}`,
+                method: "POST",
+                data: parcelInfo
+            })
+        }),
+        parcelBlock: builder.mutation({
+            invalidatesTags: ["PARCEL"],
+            query: ({ id, parcelInfo }) => ({
+                url: `/parcels/block/${id}`,
+                method: "POST",
+                data: parcelInfo
+            })
+        }),
     })
 });
 
-export const { useTrackQuery } = parcelApi;
+export const { useTrackQuery, useGetAllParcelQuery, useParcelStatusChangeMutation, useParcelBlockMutation } = parcelApi;
